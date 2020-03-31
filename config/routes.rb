e0068f579eaf0caf92827fda91e2bccabe71018e
vladveterok <vl.berezhnoi@gmail.com>
2020-03-31 18:11:32 +0300

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new' 
+  end
+  get 'store/index'
+  root 'store#index'
+end
