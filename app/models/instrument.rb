5ba54f9efc9031feb90d83b14e6419eca87c1ecc
vladveterok <vl.berezhnoi@gmail.com>
2020-04-01 19:31:43 +0300

    added cart and line_items

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,8 +1,10 @@
 class Instrument < ApplicationRecord
+  before_destroy :not_referenced_by_any_line_item
   mount_uploader :image, ImageUploader
   serialize :image, JSON # if you use SQLite, add this line
 
   belongs_to :user, optional: true
+  has_many :line_items
 
   validates :title, :brand, :price, :model, presence: true
   validates :description, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed." }
@@ -12,4 +14,13 @@ class Instrument < ApplicationRecord
   BRAND = %w{ Fender Gibson Epiphone ESP Martin Dean Taylor Jackson PRS  Ibanez Charvel Washburn }
   FINISH = %w{ Black White Navy Blue Red Clear Satin Yellow Seafoam }
   CONDITION = %w{ New Excellent Mint Used Fair Poor }
+
+  private
+
+  def not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line items present")
+      throw :abort
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
   resources :instruments
   devise_for :users, controllers: { registrations: 'registrations' }
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new' 
   end
-  get 'store/index'
+  # get 'store/index'
   root 'instruments#index'
 end
